8133ca834dd725c94d727d8338f1d65b399e7a3a
ANLU0108 <andre.lundahl@gmail.com>
2015-10-21 15:42:58 +0200

    Google Maps Fragment - Basics working

--- app/src/main/java/se/hkr/anlu0108/transportaa/Login.java
+++ app/src/main/java/se/hkr/anlu0108/transportaa/Login.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class Login extends AppCompatActivity {
+public class Login extends Activity {
 
     EditText username;
     EditText password;
